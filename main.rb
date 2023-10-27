8d08c86b93ce1044bb61cc5de6379d25f757e058
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-10-27 15:18:29 +0200

    create run bash script to fiddle with working dir

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
-#!/usr/bin/env ruby
 require_relative "src/runner"
+
+# Change to the directory the user ran this script from. See `run`.
+Dir.chdir(ARGV[0])
 
 runner = Runner.new
 runner.load_initial_data
