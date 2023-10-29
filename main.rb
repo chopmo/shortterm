4be9db973803a39d63c5a7dd2dc2ba726d8112fd
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-10-29 08:50:04 +0100

    Fix caching dir

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
 require_relative "src/runner"
 
-# Change to the directory the user ran this script from. See `run`.
-Dir.chdir(ARGV[0])
-
 runner = Runner.new
 runner.load_initial_data
 runner.loop
--- src/git.rb
+++ src/git.rb
@@ -13,37 +13,47 @@ class Git
   end
 
   def self.start_or_switch_to_story(story)
-    if !working_tree_clean?
-      puts "The working tree is not clean."
-      wait_for_key
-      return false
-    end
+    with_current_dir(ARGV[0]) do
+      if !working_tree_clean?
+        puts "The working tree is not clean."
+        wait_for_key
+        return false
+      end
 
-    if story.branches.empty?
-      branch = branch_name(story)
-      `git co main`
-      `git pull`
-      `git co -b #{branch}`
-      `git push -u`
-      puts "Done. Created and pushed new branch #{branch}"
-      return true
-    elsif story.branches.size == 1
-      branch = story.branches[0].name
-      `git co main`
-      `git pull`
-      `git fetch`
-      `git co #{branch}`
-      puts "Done. Switched to existing branch #{branch}"
-      return true
-    else
-      puts "The story already has branches."
-      wait_for_key
-      return false
+      if story.branches.empty?
+        branch = branch_name(story)
+        `git co main`
+        `git pull`
+        `git co -b #{branch}`
+        `git push -u`
+        puts "Done. Created and pushed new branch #{branch}"
+        return true
+      elsif story.branches.size == 1
+        branch = story.branches[0].name
+        `git co main`
+        `git pull`
+        `git fetch`
+        `git co #{branch}`
+        puts "Done. Switched to existing branch #{branch}"
+        return true
+      else
+        puts "The story already has branches."
+        wait_for_key
+        return false
+      end
     end
   end
 
   def self.wait_for_key
     puts "Press any key..."
     STDIN.getch
+  end
+
+  def self.with_current_dir(dir)
+    old_dir = Dir.getwd
+    Dir.chdir(dir)
+    yield
+  rescue
+    Dir.chdir(old_dir)
   end
 end
